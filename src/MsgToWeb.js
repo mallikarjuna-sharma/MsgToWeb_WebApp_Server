8807ded37ea639557f03718711154698cf707ecb
mallikarjuna sharma <rmalliksharma96@gmail.com>
2020-04-05 18:01:16 +0530

    Update MsgToWeb.js

--- src/MsgToWeb.js
+++ src/MsgToWeb.js
@@ -63,7 +63,7 @@ export default class MsgToWeb extends Component {
                     <div style={ (this.state.showRemoveEfect) ?  styles.lightSpeedOut :styles.lightSpeedIn} >
                         <h1 style={{
                             "textShadow":"0 1px 0 #ccc,                0 2px 0 #c9c9c9,                0 3px 0 #bbb,                0 4px 0 #b9b9b9,                0 5px 0 #aaa,                0 6px 1px rgba(0,0,0,.1),                0 0 5px rgba(0,0,0,.1),                0 1px 3px rgba(0,0,0,.3),                0 3px 5px rgba(0,0,0,.2),                0 5px 10px rgba(0,0,0,.25),                0 10px 10px rgba(0,0,0,.2),                0 20px 20px rgba(0,0,0,.15)",
-                            paddingLeft: "35%", color: "white", paddingTop: "5%",fontSize:80
+                            paddingLeft: "25%", color: "white", paddingTop: "5%",fontSize:80
                         }}>{this.state.message}</h1>
                     </div>
                 </StyleRoot>
